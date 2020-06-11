99bc93382e308e441cb9181e632af27fc5f441cd
Jordan Trickett <jordan1trickett@gmail.com>
2020-06-11 17:54:15 +0100

    resolver for user set up

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,6 +4,16 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if there is a current user id
+    if (!ctx.request.userID) return null;
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userID },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
